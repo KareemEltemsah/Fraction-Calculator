5fd5dd9525ade04200b259d311aad9be886725e0
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2021-05-09 18:35:24 +0200

    fraction reducing
    function to reduce the fraction integers

--- FractionCalculator/Fraction.cpp
+++ FractionCalculator/Fraction.cpp
@@ -12,12 +12,14 @@ Fraction::Fraction(int n, int d)
 {
     numerator = n;
     denominator = d;
+    this -> reduce();
 }
 Fraction Fraction::operator + (Fraction f)
 {
     Fraction result;
     result.numerator = (numerator * f.denominator) + (f.numerator * denominator);
     result.denominator = denominator * f.denominator;
+    result.reduce();
     return result;
 }
 Fraction Fraction::operator - (Fraction f)
@@ -25,6 +27,7 @@ Fraction Fraction::operator - (Fraction f)
     Fraction result;
     result.numerator = (numerator * f.denominator) - (f.numerator * denominator);
     result.denominator = denominator * f.denominator;
+    result.reduce();
     return result;
 }
 Fraction Fraction::operator * (Fraction f)
@@ -32,6 +35,7 @@ Fraction Fraction::operator * (Fraction f)
     Fraction result;
     result.numerator = numerator * f.numerator;
     result.denominator = denominator * f.denominator;
+    result.reduce();
     return result;
 }
 Fraction Fraction::operator / (Fraction f)
@@ -39,6 +43,7 @@ Fraction Fraction::operator / (Fraction f)
     Fraction result;
     result.numerator = numerator * f.denominator;
     result.denominator = denominator * f.numerator;
+    result.reduce();
     return result;
 }
 bool Fraction::operator < (Fraction f)
@@ -66,14 +71,26 @@ bool Fraction::operator == (Fraction f)
     int y = (numerator * f.denominator) - (f.numerator * denominator);
     return (y == 0) ? true : false;
 }
-std::istream& operator>> (std::istream& in , Fraction& f)
+std::istream& operator>> (std::istream& in, Fraction& f)
 {
     char c;
     cin >> f.numerator >> c >> f.denominator;
     return in;
 }
-std::ostream& operator<< (std::ostream& out , Fraction f)
+std::ostream& operator<< (std::ostream& out, Fraction f)
 {
     cout << f.numerator << " / " << f.denominator;
     return out;
+}
+void Fraction::reduce ()
+{
+    for (int i=max(numerator, denominator) ; i >= 2 ; i--)
+    {
+        if (numerator % i == 0 && denominator % i == 0)
+        {
+            numerator /= i;
+            denominator /= i;
+            break;
+        }
+    }
 }
