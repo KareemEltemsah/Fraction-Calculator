0a6f991c369fa8ae7d19f9caae997792b0d8524c
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2021-05-09 17:56:04 +0200

    comparing operators
    finished all comparisons (<, >, <=, >=, ==)

--- FractionCalculator/Fraction.cpp
+++ FractionCalculator/Fraction.cpp
@@ -27,19 +27,44 @@ Fraction Fraction::operator - (Fraction f)
     result.denominator = denominator * f.denominator;
     return result;
 }
-Fraction Fraction::operator* (Fraction f)
+Fraction Fraction::operator * (Fraction f)
 {
     Fraction result;
     result.numerator = numerator * f.numerator;
     result.denominator = denominator * f.denominator;
     return result;
 }
-Fraction Fraction::operator/ (Fraction f)
+Fraction Fraction::operator / (Fraction f)
 {
     Fraction result;
     result.numerator = numerator * f.denominator;
     result.denominator = denominator * f.numerator;
     return result;
+}
+bool Fraction::operator < (Fraction f)
+{
+    int y = (numerator * f.denominator) - (f.numerator * denominator);
+    return (y < 0) ? true : false;
+}
+bool Fraction::operator > (Fraction f)
+{
+    int y = (numerator * f.denominator) - (f.numerator * denominator);
+    return (y > 0) ? true : false;
+}
+bool Fraction::operator <= (Fraction f)
+{
+    int y = (numerator * f.denominator) - (f.numerator * denominator);
+    return (y <= 0) ? true : false;
+}
+bool Fraction::operator >= (Fraction f)
+{
+    int y = (numerator * f.denominator) - (f.numerator * denominator);
+    return (y >= 0) ? true : false;
+}
+bool Fraction::operator == (Fraction f)
+{
+    int y = (numerator * f.denominator) - (f.numerator * denominator);
+    return (y == 0) ? true : false;
 }
 std::istream& operator>> (std::istream& in , Fraction& f)
 {
--- FractionCalculator/main.cpp
+++ FractionCalculator/main.cpp
@@ -11,5 +11,10 @@ int main()
     cout << a - b << endl;
     cout << a * b << endl;
     cout << a / b << endl;
+    cout << (a < b) << endl;
+    cout << (a > b) << endl;
+    cout << (a <= b) << endl;
+    cout << (a >= b) << endl;
+    cout << (a == b) << endl;
     return 0;
 }
